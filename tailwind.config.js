7ec5230032b2a79eea11c87e125e248cdfce167d
zhenglingpeng <lingzhengpeng@Outlook.com>
2024-08-07 16:55:33 +0800

    style :taiwindcss default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 /** @type {import('tailwindcss').Config} */ 
 export default { 
+  darkMode: 'class',
   content: [ "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ], 
   theme: { 
       extend: {}, 
