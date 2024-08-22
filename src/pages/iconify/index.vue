835284da7d4d1c3829a57ba4db10ec152fc1c219
zhenglingpeng <lingzhengpeng@Outlook.com>
2024-08-22 10:31:52 +0800

    你的提交信息

--- src/pages/iconify/index.vue
+++ src/pages/iconify/index.vue
@@ -1,19 +1,43 @@
 <template>
-     <container>
-        <div class="grid grid-cols-3 gap-4">
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="teenyicons:search-outline" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="tdesign:arrow-down-circle" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="tdesign:address-book" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="tdesign:folder-add" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="ei:like" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="ei:sc-github" class="w-8 h-8 "></Icon></div>
-            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center"><Icon icon="ei:image" class="w-8 h-8 "></Icon></div>
-        </div>
-     </container>
+  <container>
+    <div class="mb-4">
+      <span class="font-bold text-base">iconify海量资源: </span>
+      <a class="text-blue-400" target="_blank" href="https://icon-sets.iconify.design/?category=General">https://icon-sets.iconify.design/?category=General</a>
+    </div>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="teenyicons:search-outline" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="tdesign:arrow-down-circle" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="tdesign:address-book" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="tdesign:folder-add" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="ei:like" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="ei:sc-github" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="ei:image" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="ic:baseline-account-circle" class="h-1/2 w-1/2"></Icon>
+      </div>
+      <div class="flex aspect-square w-full items-center justify-center bg-blue-200">
+        <Icon icon="ic:baseline-alarm-on" class="h-1/2 w-1/2"></Icon>
+      </div>
+    </div>
+  </container>
 </template>
 
 <script setup lang="ts">
- import { Icon } from '@iconify/vue';
+import { Icon } from '@iconify/vue';
 definePage({
   name: 'iconify',
   meta: {
@@ -21,8 +45,6 @@ definePage({
     title: 'iconify',
     i18n: 'home.iconify',
   },
-})
-   
+});
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- src/pages/unlimitedLists/index.vue
+++ src/pages/unlimitedLists/index.vue
@@ -1,55 +1,71 @@
 <template>
-<container>
-  <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
-  <van-list
-    v-model:loading="loading"
-    :finished="finished"
-    finished-text="没有更多了"
-    @load="onLoad"
-  >
-    <van-cell v-for="item in list" :key="item" :title="item" />
-  </van-list>
-</van-pull-refresh>
-</container>
-
+  <container>
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+      <van-list
+        v-model:loading="listLoading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <div v-for="(item, index) in listData" :key="index" :title="item" class="w-full rounded mt-5">
+          <div class="flex">
+            <div class="w-28 h-28 bg-blue-200 flex justify-center	items-center">{{ item.img }}</div>
+          <div>{{ item.title }}</div>
+          </div>
+          
+        </div>
+      </van-list>
+    </van-pull-refresh>
+  </container>
 </template>
 <script setup lang="ts">
-import {getList} from '@/api/service/unlimitList'
+import { getList } from '@/api/service/unlimitList';
+import { useInfiniteQuery, useQuery } from '@tanstack/vue-query';
 definePage({
-  name:'unlimitedLists',
+  name: 'unlimitedLists',
   meta: {
     level: 2,
     title: '无限加载',
     i18n: 'home.unlimitedLists',
+  },
+});
+const finished = ref(false);
+const refreshing = ref(false);
+const query = reactive({
+  page: 1,
+  limit: 10,
+});
+
+const onLoad = () => {
+  // if()
+  query.page += 1;
+  console.log(listData,"2323");
+};
+// const { data: listData, isLoading:listLoading } = useQuery({
+//   queryKey: ['unlimitedLists'],
+//   queryFn: async () => {
+//     const res = await getList(query);
+//     return res;
+//   },
+  
+// });
+const { data, fetchNextPage, hasNextPage, isFetchingNextPage,status} = useInfiniteQuery({
+  queryKey: ['unlimitedLists'],
+  queryFn: async (query) => getList(query),
+  getNextPageParam: (lastPage,pages) => {
+    if(lastPage.length < query.limit) {
+      return false
+    }
   }
 })
-    const list = ref([]);
-    const loading = ref(false);
-    const finished = ref(false);
-    const refreshing = ref(false);
-    const query = reactive({
-      page:1,
-      limit:10,
-    });
-    const onLoad = () => {
-      getList(query).then((res) => {
-        if (refreshing.value) {
-          list.value = [];
-          refreshing.value = false;
-        }
-        list.value = list.value.concat(res.data);
-        loading.value = false;
-      })
-    };
+const onRefresh = () => {
+  // 清空列表数据
+  finished.value = false;
 
-    const onRefresh = () => {
-      // 清空列表数据
-      finished.value = false;
+  // 重新加载数据
+  // 将 loading 设置为 true，表示处于加载状态
 
-      // 重新加载数据
-      // 将 loading 设置为 true，表示处于加载状态
-      loading.value = true;
-      onLoad();
-    };
+  query.page = 1;
+};
 </script>
 <style scoped></style>
